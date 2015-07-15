e47ee5471f289ddeda267631b4437c015d978e9f
1130838 <1130838@isep.ipp.pt>
2015-07-15 16:46:03 +0100

    Initial commit

---
+++ DP1_Duck/src/model/Duck.java
@@ -0,0 +1,16 @@
+package model;
+
+/**
+ * Created by brunodevesa on 15/07/15.
+ */
+public class Duck {
+
+    String name;
+    int age;
+
+    public Duck(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+}
---
+++ DP1_Duck/src/presentation/MainUI.java
@@ -0,0 +1,12 @@
+package presentation;
+
+/**
+ * Created by brunodevesa on 15/07/15.
+ */
+public class MainUI {
+
+    public void run() {
+        System.out.println("beggining..");
+    }
+
+}
